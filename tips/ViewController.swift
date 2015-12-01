caa2d3351b4471bad522fc28875b0c490e8cc018
Himank Yadav <him229@gmail.com>
2015-11-30 20:04:34 -0600

    Added some UI

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text="$0.00"
+        totalLabel.text="$0.00"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var billAmountStr = billField.text!
+        var billAmount = billAmountStr._bridgeToObjectiveC().doubleValue
+        var tip = 0.2 * billAmount
+        var total = tip + billAmount
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+
+        
+
+    }
 
 }
 
