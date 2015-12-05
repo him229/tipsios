1d760561933bda7b713ac6335a2d8e82b046a8be
Himank Yadav <him229@gmail.com>
2015-12-05 02:01:14 -0600

    Added UI for PayVC

--- tips/PayViewController.swift
+++ tips/PayViewController.swift
@@ -20,17 +20,43 @@ class PayViewController: UIViewController {
     
     // MARK: - Text field delegate
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         
         return true
     }
     
-    
     // MARK: Actions
     
     @IBAction func donate(sender: AnyObject) {
         
+        var stripCard = STPCard()
         
+        // Split the expiration date to extract Month & Year
+        if self.expireDateTextField.text!.isEmpty == false {
+            let expirationDate = self.expireDateTextField.text!.componentsSeparatedByString("/")
+            let expMonth = UInt(Int(expirationDate[0])!)
+            let expYear = UInt(Int(expirationDate[1])!)
+            
+            // Send the card info to Strip to get the token
+            stripCard.number = self.cardNumberTextField.text
+            stripCard.cvc
+                = self.cvcTextField.text
+            stripCard.expMonth = expMonth
+            stripCard.expYear = expYear
+        }
     }
     
 }
