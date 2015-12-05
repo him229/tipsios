4d5ad4fb587142a567395889e4b94efac7739bad
Himank Yadav <him229@gmail.com>
2015-12-05 01:12:40 -0600

    Added UI for PayVC

--- tips/PayViewController.swift
+++ tips/PayViewController.swift
@@ -8,28 +8,30 @@
 
 import UIKit
 
+
 class PayViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var cardNumberTextField: UITextField!
+    @IBOutlet weak var expireDateTextField: UITextField!
+    @IBOutlet weak var cvcTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    //@IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    // MARK: - Text field delegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        return true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: Actions
+    
+    @IBAction func donate(sender: AnyObject) {
+        
+        
     }
-    */
-
+    
 }
+
