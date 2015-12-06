74b11c927fa82f6ddccb8d58886a2bf9c89ce841
Himank Yadav <him229@gmail.com>
2015-12-05 19:02:19 -0600

    PayVC error resolved

--- tips/PayViewController.swift
+++ tips/PayViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 
 class PayViewController: UIViewController {
-
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var cardNumberTextField: UITextField!
-    @IBOutlet weak var expireDateTextField: UITextField!
-    @IBOutlet weak var cvcTextField: UITextField!
-    @IBOutlet weak var amountTextField: UITextField!
+//
+//    @IBOutlet weak var emailTextField: UITextField!
+//    @IBOutlet weak var cardNumberTextField: UITextField!
+//    @IBOutlet weak var expireDateTextField: UITextField!
+//    @IBOutlet weak var cvcTextField: UITextField!
+//    @IBOutlet weak var amountTextField: UITextField!
+    
+    
     //@IBOutlet weak var spinner: UIActivityIndicatorView!
     
     // MARK: - Text field delegate
@@ -32,32 +34,32 @@ class PayViewController: UIViewController {
     }
 
     
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        
-        return true
-    }
-    
-    // MARK: Actions
-    
-    @IBAction func donate(sender: AnyObject) {
-        
-        var stripCard = STPCard()
-        
-        // Split the expiration date to extract Month & Year
-        if self.expireDateTextField.text!.isEmpty == false {
-            let expirationDate = self.expireDateTextField.text!.componentsSeparatedByString("/")
-            let expMonth = UInt(Int(expirationDate[0])!)
-            let expYear = UInt(Int(expirationDate[1])!)
-            
-            // Send the card info to Strip to get the token
-            stripCard.number = self.cardNumberTextField.text
-            stripCard.cvc
-                = self.cvcTextField.text
-            stripCard.expMonth = expMonth
-            stripCard.expYear = expYear
-        }
-    }
+//    
+//    func textFieldShouldReturn(textField: UITextField) -> Bool {
+//        
+//        return true
+//    }
+//    
+//    // MARK: Actions
+//    
+//    @IBAction func donate(sender: AnyObject) {
+//        
+//        var stripCard = STPCard()
+//        
+//        // Split the expiration date to extract Month & Year
+//        if self.expireDateTextField.text!.isEmpty == false {
+//            let expirationDate = self.expireDateTextField.text!.componentsSeparatedByString("/")
+//            let expMonth = UInt(Int(expirationDate[0])!)
+//            let expYear = UInt(Int(expirationDate[1])!)
+//            
+//            // Send the card info to Strip to get the token
+//            stripCard.number = self.cardNumberTextField.text
+//            stripCard.cvc
+//                = self.cvcTextField.text
+//            stripCard.expMonth = expMonth
+//            stripCard.expYear = expYear
+//        }
+//    }
     
 }
 
---
+++ tips/PayingViewController.swift
@@ -0,0 +1,70 @@
+//
+//  PayingViewController.swift
+//  tips
+//
+//  Created by Himank Yadav on 12/5/15.
+//  Copyright © 2015 himankyadav. All rights reserved.
+//
+
+import UIKit
+
+class PayingViewController: UIViewController {
+    
+        @IBOutlet weak var emailTextField: UITextField!
+        @IBOutlet weak var cardNumberTextField: UITextField!
+        @IBOutlet weak var expireDateTextField: UITextField!
+        @IBOutlet weak var cvcTextField: UITextField!
+        @IBOutlet weak var amountTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+        func textFieldShouldReturn(textField: UITextField) -> Bool {
+    
+            return true
+        }
+    
+        // MARK: Actions
+    
+        @IBAction func donate(sender: AnyObject) {
+    
+            var stripCard = STPCard()
+    
+            // Split the expiration date to extract Month & Year
+            if self.expireDateTextField.text!.isEmpty == false {
+                let expirationDate = self.expireDateTextField.text!.componentsSeparatedByString("/")
+                let expMonth = UInt(Int(expirationDate[0])!)
+                let expYear = UInt(Int(expirationDate[1])!)
+    
+                // Send the card info to Strip to get the token
+                stripCard.number = self.cardNumberTextField.text
+                stripCard.cvc
+                    = self.cvcTextField.text
+                stripCard.expMonth = expMonth
+                stripCard.expYear = expYear
+            }
+        }
+
+}
